a15e1bc93d5d615d805869771637c88f038ca9cf
daniel1698 <68821566+daniel1698@users.noreply.github.com>
2021-02-01 01:27:24 -0300

    parte 5

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-$('.slider-principal').slick()
+
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
